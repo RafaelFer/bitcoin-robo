03903ce0997201f0624b4f7f33461a7bfa03d124
Rafael <rafael22fs@gmail.com>
2019-08-01 18:55:04 -0300

    adicionando auth

---
+++ robo/src/main/java/br/com/bitcoin/robo/controller/AutenticacaoController.java
@@ -0,0 +1,18 @@
+package br.com.bitcoin.robo.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/auth")
+public class AutenticacaoController {
+
+    @PostMapping
+    public ResponseEntity<?> autenticar() {
+            return ResponseEntity.ok().build();
+    }
+}
